2ef6253588f60ec53abc580644ef15721f9550a4
Elkin Urango <kin3xc@hotmail.com>
2018-01-15 17:39:37 -0500

    Mostrar datos en el web site

--- tracker.php
+++ tracker.php
@@ -87,9 +87,9 @@
                               });
                         marker.setMap(map);
                         map.setCenter(new google.maps.LatLng(words[1],words[2]));
-                        document.getElementById('time').innerHTML=words[0] || "";
-                        document.getElementById('lat').innerHTML=words[1] || "";
-                        document.getElementById('lon').innerHTML=words[2] || "";
+                        document.getElementById('time').innerHTML=words[0];
+                        document.getElementById('lat').innerHTML=words[1];
+                        document.getElementById('lon').innerHTML=words[2];
                     }
                 }
                 marker.setAnimation(google.maps.Animation.BOUNCE);
