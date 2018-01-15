d488ce6e8b54985b6386115e08d28f7fab953c38
Elkin Urango <kin3xc@hotmail.com>
2018-01-15 13:20:45 -0500

    Script ready for GET and POST

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// configurar body parser para leer el cuepor de las peticiones
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', ( req, res )=>{
+	res.status(200);
+	res.send("Hola, estas conectado al servidor de pruebas");
+});
+
+app.post('/', ( req, res )=>{
+	console.log(req.body);
+	res.status(200);
+	res.json({mensaje:"Datos recibidos", data:req.body});
+});
+
+// para servir archivos estaticos
+// app.use(express.static('public'));
+
+// levanta el server
+app.listen(4201, function(){
+	console.log('Estoy listo para recibir peticiones');
+});
